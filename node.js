5168a89c57caf94526d6d5b146a64b2ebbd239d7
harshit saxena <harshitsaxena805@gmail.com>
2022-02-10 20:28:56 +0530

    second commit

---
+++ node.js
@@ -0,0 +1,16 @@
+// Create a function for reusable perpose
+const generateRandomString = (myLength) => {
+  const chars =
+    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  const randomArray = Array.from(
+    { length: myLength },
+    (v, k) => chars[Math.floor(Math.random() * chars.length)]
+  );
+
+  const randomString = randomArray.join("");
+  return randomString;
+};
+
+// Try it
+console.log(generateRandomString(10));
+console.log(generateRandomString(30));
